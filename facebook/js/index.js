f21d1397f36e46fea76f1db03d770e941cc66076
Chaeyun <pcpclee1@naver.com>
2017-01-17 21:06:06 +0900

    no message

--- facebook/js/index.js
+++ facebook/js/index.js
@@ -1,7 +1,9 @@
-var errorMSG = "글자수가 초과되었습니다!";
-
+var errorMSG = "글자수(20)를 초과했습니다!";
+var max = 20;
 var black_button = document.querySelector("#black_button");
 var white_button = document.querySelector("#white_button");
+var writebox = document.querySelector("#writebox");
+var cancel = document.querySelector("#cancel");
 
 black_button.addEventListener("click",function(){
   document.querySelector("body").style.backgroundColor = "black";
@@ -9,9 +11,6 @@ black_button.addEventListener("click",function(){
 white_button.addEventListener("click",function(){
   document.querySelector("body").style.backgroundColor = "#e9ebee";
 });
-
-var writebox = document.querySelector("#writebox");
-var cancel = document.querySelector("#cancel");
 
 writebox.addEventListener("click",function(){
   document.querySelector("#option").style = "display:none";
@@ -22,13 +21,11 @@ cancel.addEventListener("click",function(){
   document.querySelector("#option2").style = "display:none"
 });
 
-var errorMSG = "글자수(20)를 초과했습니다!";
-var max = 20;
 function checkStringMax(string){
-    max = 20;
     if(string.length >= max) return true;
     return false;
 }
+
 writebox.addEventListener("keydown",function(){
   if(checkStringMax(writebox.value)){
     alert(errorMSG);
