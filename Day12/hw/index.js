b1343e5f016249ab0b77cb18f5dec66186890141
Chaeyun <pcpclee1@naver.com>
2017-01-19 00:17:47 +0900

    숙제 1번 업로드

---
+++ Day12/hw/index.js
@@ -0,0 +1,22 @@
+
+var first = document.getElementById("first");
+var second = document.getElementById("second");
+var third = document.getElementById("third");
+
+first.addEventListener("click",function(evt){
+  var target = evt.target;
+  console.log("first: ",target);
+});
+second.addEventListener("click",function(evt){
+  var target = evt.target;
+  console.log("second: ",target);
+});
+third.addEventListener("click",function(evt){
+  var target = evt.target;
+  console.log("third: ",target);
+});
+
+document.addEventListener("click",function(evt){
+  var target = evt.target;
+  console.log("target: ", target);
+});
