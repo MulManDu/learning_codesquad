bec49ffd5cf2ef7c6c5d30c63e8c22dd219b0053
Chaeyun <pcpclee1@naver.com>
2017-01-27 22:15:24 +0900

    무한 롤링 배너. 함수 수정.

--- Day16/rolling/js/main.js
+++ Day16/rolling/js/main.js
@@ -1,19 +1,28 @@
-var Btn = document.querySelector("#Btn");
-Btn.addEventListener("click", getRoll);
+function makeEvt(){
+  document.addEventListener("click", getRoll);
+}
 
 function getRoll(evt){
-  var targetId = evt.target.id;
-  Roll(targetId);
+  var target = evt.target;
+  var targetId = target.id;
+  if(target.parentNode.id !== "Btn") return;
+  moveContainer(targetId);
   moveNodes(targetId);
 }
 
-function Roll(targetId){
+function moveContainer(targetId){
   var rolling = document.querySelector("#rolling");
-  var firstContent = rolling.querySelector("div:first-child");
-  var translate = getTranslateValue(rolling);
-  var width = getContentWidth(firstContent);
-  translate = calMovement(translate, targetId, width);
-  moveAside(translate);
+  var translate = calMovement(targetId, rolling);
+  rolling.style.transform = "translate3d("+ translate +"px, 0px, 0px)";
+}
+
+function calMovement(direction, ele){
+  var firstContent = ele.querySelector("div:first-child");
+  var translate = getTransX(ele);
+  var width = getWidth(firstContent);
+  if(direction === "left") translate = translate + width;
+  else if(direction === "right") translate = translate - width;
+  return translate;
 }
 
 function moveNodes(direction){
@@ -25,16 +34,16 @@ function moveNodes(direction){
 }
 
 function moveRight(rolling, firstNode, lastNode){
-  var styleX = getTranslateValue(lastNode);
-  var width = getContentWidth(firstNode);
+  var styleX = getTransX(lastNode);
+  var width = getWidth(firstNode);
   styleX = styleX + width;
   changeStyleX(firstNode, styleX);
   rolling.appendChild(firstNode);
 }
 
 function moveLeft(rolling, firstNode, lastNode){
-  var styleX = getTranslateValue(firstNode);
-  var width = getContentWidth(firstNode);
+  var styleX = getTransX(firstNode);
+  var width = getWidth(firstNode);
   styleX = styleX - width;
   changeStyleX(lastNode, styleX);
   rolling.insertBefore(lastNode, rolling.childNodes[0]);
@@ -44,27 +53,20 @@ function changeStyleX(node, styleX){
   node.style.transform = "translate3d("+ styleX +"px, 0px, 0px)";
 }
 
-function getTranslateValue(ele){
+function getTransX(ele){
   var str = ele.style.transform;
   var translate = str.replace(/\w+\((-?\d+)px.+/,"$1");
   var translate = parseInt(translate);
   return translate;
 }
 
-function getContentWidth(ele){
+function getWidth(ele){
   var str = window.getComputedStyle(ele).getPropertyValue("width");
   var width = str.replace(/(\d+)px/,"$1");
   width = parseInt(width);
   return width;
 }
 
-function calMovement(translate, direction, width){
-  if(direction === "left") translate = translate + width;
-  else if(direction === "right") translate = translate - width;
-  return translate;
-}
-
-function moveAside(translate){
-  var rolling = document.querySelector("#rolling");
-  rolling.style.transform = "translate3d("+ translate +"px, 0px, 0px)";
-}
+document.addEventListener("DOMContentLoaded", function(){
+  makeEvt();
+});
